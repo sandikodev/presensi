acf015c910b8ac03ed56bcb9bdc3c9fd899d0fb5
13145rs18 <13145rs18@gmail.com>
2024-07-14 04:16:42 +0000

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <header>
+                <h1>selamat datang di aplikasi presensi </h1>
+            </header>
+            <section></section>
+            <footer>
+                <p>created by sandikodev</p>
+            </footer>
+        </main>
+    );
+}
